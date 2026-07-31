01201330080bf1e7c7a15cd049440adecd5727fc
Botsiture <bismillaha443@gmail.com>
2026-08-01 03:19:26 +0530

    Update pay.py

--- shivu/modules/pay.py
+++ shivu/modules/pay.py
@@ -52,7 +52,7 @@ async def pay_callback(update: Update, context: CallbackContext):
         return await q.answer()
 
     await user_collection.update_one({'id': receiver_id}, {'$inc': {'balance': amount}}, upsert=True)
-    await q.edit_message_text(f"<b>ᴘᴀʏᴍᴇɴᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ!</b>\n\nʏᴏᴜ ꜱᴇɴᴛ 💸 <b>{amount}</b> <b>ᴄᴏɪɴꜱ.</b>")              
+    await q.edit_message_text(f"ᴘᴀʏᴍᴇɴᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ! \nʏᴏᴜ ꜱᴇɴᴛ 💸 {amount} ᴄᴏɪɴꜱ ᴛᴏ ɢᴀʀᴇᴇʙ")              
     await q.answer()
 
 
