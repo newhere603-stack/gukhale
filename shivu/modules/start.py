3ee6ad86a20e9cc2059b6781135d74eaf0e5922d
Botsiture <bismillaha443@gmail.com>
2026-07-31 15:09:01 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -106,7 +106,7 @@ async def credits_view(context: CallbackContext):
     for name, username, user_id in CREDITS_USERS:
         url = f'tg://user?id={user_id}'
         kb.append([InlineKeyboardButton(f"{name}", url=url)])
-    kb.append([InlineKeyboardButton("BACK", callback_data='sxc_back')])
+    kb.append([InlineKeyboardButton("⟲ ʙᴀᴄᴋ", callback_data='sxc_back')])
     return "𝗦𝘂𝗱𝗼:", InlineKeyboardMarkup(kb)
 def _new_user_doc(user_id, first_name, username):
     return {
