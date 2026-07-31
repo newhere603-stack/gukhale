f61622691ad27fe9f9671c0d8b256f599ab14a80
Botsiture <bismillaha443@gmail.com>
2026-07-31 15:23:02 +0530

    Update explore.py

--- shivu/modules/explore.py
+++ shivu/modules/explore.py
@@ -85,8 +85,8 @@ async def explore_cmd(update: Update, context: CallbackContext) -> None:
 
         action = random.choice(EXPLORE_ACTIONS)
         await update.message.reply_text(
-            f"<b>🗺️ ʏᴏᴜ {action} ᴀɴᴅ ғᴏᴜɴᴅ {reward} ᴄᴏɪɴs!</b>\n"
-            f"<b>💸 ᴇxᴘʟᴏʀᴀᴛɪᴏɴ ғᴇᴇ: -{CONFIG.fee} ᴄᴏɪɴs</b>"
+            f"<b>🗺️ {sc(ʏᴏᴜ {action} ᴀɴᴅ ғᴏᴜɴᴅ {reward} ᴄᴏɪɴs!)}</b>\n"
+            f"<b>💸 {sc(ᴇxᴘʟᴏʀᴀᴛɪᴏɴ ғᴇᴇ: ≻{CONFIG.fee} ᴄᴏɪɴs)}</b>"
         )
 
     except Exception as e:
