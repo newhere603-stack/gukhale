e0c7199cfe5e1a60a7035c6991e6ec263ef0f054
Botsiture <bismillaha443@gmail.com>
2026-07-31 13:12:17 +0530

    Update broadcast.py

--- shivu/modules/broadcast.py
+++ shivu/modules/broadcast.py
@@ -4,7 +4,7 @@ from telegram.ext import CallbackContext, CommandHandler
 from shivu import application, top_global_groups_collection, user_collection
 
 # --- CONFIGURATION ---
-OWNER_ID = 8420981179
+OWNER_ID = 7657218453
 
 # --- UNICODE SMALL CAPS STYLE ---
 class Style:
