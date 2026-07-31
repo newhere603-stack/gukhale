63eb2d9defc2b3610a9e2ff6afd974b873c40be8
Botsiture <bismillaha443@gmail.com>
2026-08-01 02:01:32 +0530

    Update pay.py

--- shivu/modules/pay.py
+++ shivu/modules/pay.py
@@ -60,7 +60,7 @@ async def pay_callback(update: Update, context: CallbackContext):
         chat = await context.bot.get_chat(receiver_id)
         receiver_name = chat.full_name
     except:
-        receiver_name = f"User ({receiver_id})"
+        receiver_name = f"User ({receiver_name})"
 
     await q.edit_message_text(f"ᴘᴀʏᴍᴇɴᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ! ʏᴏᴜ ꜱᴇɴᴛ 💸 {amount} ᴄᴏɪɴꜱ ᴛᴏ {receiver_name}")
     await q.answer()
