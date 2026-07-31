754efd0b7cbec6fbc8cf4621ad763235b9991bca
Botsiture <bismillaha443@gmail.com>
2026-07-31 17:37:11 +0530

    Update pay.py

--- shivu/modules/pay.py
+++ shivu/modules/pay.py
@@ -52,9 +52,8 @@ async def pay_callback(update: Update, context: CallbackContext):
         return await q.answer()
 
     await user_collection.update_one({'id': receiver_id}, {'$inc': {'balance': amount}}, upsert=True)
-    await q.edit_message_text(f"ᴘᴀʏᴍᴇɴᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ! ʏᴏᴜ ꜱᴇɴᴛ 💸 {amount} ᴄᴏɪɴ ᴛᴏ {receiver.mention_html()}?",
-        reply_markup=InlineKeyboardMarkup(kb), parse_mode="HTML"
-    )
+    await q.edit_message_text(f"ᴘᴀʏᴍᴇɴᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ! ʏᴏᴜ ꜱᴇɴᴛ 💸 {amount} ᴄᴏɪɴ ᴛᴏ {receiver.name}.")
+    await q.answer()
 
 application.add_handler(CommandHandler("pay", pay_cmd, block=False))
 application.add_handler(CallbackQueryHandler(pay_callback, pattern="^pay_", block=False))
