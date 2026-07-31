4e10c76b876ad2d69da59e5c39640d03a2270264
Botsiture <bismillaha443@gmail.com>
2026-07-31 13:59:58 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,12 +5,12 @@ class Config(object):
     OWNER_ID = "7657218453"
     sudo_users = ["7657218453", "7657218453"]
     GROUP_ID = "-1003087506512"
-    TOKEN = "8785412311:AAEQ9cQCV_G2_p9Ch9BuEItn-OITP4WS3Mg"
+    TOKEN = "8917618422:AAEUhfTPSXFb0dDbavpMnx9VHxD4szCuba0"
     mongo_url = "mongodb+srv://alisawaifubot_db_user:jxVLyqL2QxfWSZ6Q@cluster0.hdzxkt6.mongodb.net/?appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/sgo9in.png", "https://files.catbox.moe/kgcrnb.jpeg"]
     SUPPORT_CHAT = "ANIME_GROUP_HAI"
     UPDATE_CHAT = "SAND_VILLAGE"
-    BOT_USERNAME = "AlisaWaifusBot"
+    BOT_USERNAME = "AlyaWaifuBot"
     CHARA_CHANNEL_ID = "-1004310255455"
     api_id = "10658015"
     api_hash = "a0087bca748f86698c53d291c9e5b3af"
