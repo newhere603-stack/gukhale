e20d4265294a6acb939c8ee793a8cf7a5f25a7a8
Botsiture <bismillaha443@gmail.com>
2026-08-01 06:54:32 +0530

    Update db.py

--- shivu/Database/db.py
+++ shivu/Database/db.py
@@ -1,30 +1,38 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-# Environment variables se Mongo details fetch karna
 MONGO_URI = os.getenv(
     "MONGO_URL",
     "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority",
 )
 DB_NAME = os.getenv("DB_NAME", "GRABBING_YOUR_WAIFU")
 
-# Async Motor Client connection
-client = AsyncIOMotorClient(MONGO_URI)
-db = client[DB_NAME]
-user_collection = db["users"]
+# Global variable for client
+db_client = None
 
+def get_db():
+    global db_client
+    if db_client is None:
+        # Motor client running loop ke andar initialize hoga
+        db_client = AsyncIOMotorClient(MONGO_URI)
+    return db_client[DB_NAME]
 
-async def get_user(uid):
-    return await user_collection.find_one({"id": uid})
+# Dummy wrapper or collection accessor
+class LazyCollection:
+    def __init__(self, name):
+        self.name = name
 
+    @property
+    def col(self):
+        return get_db()[self.name]
 
-async def init_user(uid):
-    user = {"id": uid, "balance": 0}
-    await user_collection.insert_one(user)
-    return user
+    async def find_one(self, *args, **kwargs):
+        return await self.col.find_one(*args, **kwargs)
 
+    async def insert_one(self, *args, **kwargs):
+        return await self.col.insert_one(*args, **kwargs)
 
-async def save_user_data(user_id, user_data):
-    await user_collection.update_one(
-        {"id": user_id}, {"$set": user_data}, upsert=True
-    )
+    async def update_one(self, *args, **kwargs):
+        return await self.col.update_one(*args, **kwargs)
+
+user_collection = LazyCollection("users")
