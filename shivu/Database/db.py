674aa8f8e28bff6ff9e1a55089afd4718f9a4525
Botsiture <bismillaha443@gmail.com>
2026-08-01 06:49:33 +0530

    Update db.py

--- shivu/Database/db.py
+++ shivu/Database/db.py
@@ -1,21 +1,19 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-# 1. MONGO_URI setup: os.getenv me pehle environment variable ka naam aata hai, default value second parameter hoti hai
+# Environment variables se Mongo details fetch karna
 MONGO_URI = os.getenv(
     "MONGO_URL",
     "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority",
 )
 DB_NAME = os.getenv("DB_NAME", "GRABBING_YOUR_WAIFU")
-COLLECTION_NAME = "users"
 
-# 2. Async Client Connection
+# Async Motor Client connection
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
-user_collection = db[COLLECTION_NAME]
+user_collection = db["users"]
 
 
-# 3. Functions ko async/await kar diya gaya hai
 async def get_user(uid):
     return await user_collection.find_one({"id": uid})
 
@@ -30,4 +28,3 @@ async def save_user_data(user_id, user_data):
     await user_collection.update_one(
         {"id": user_id}, {"$set": user_data}, upsert=True
     )
-    print("User data saved successfully.")
