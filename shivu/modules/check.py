9e11b0c2dba592c7a5aef601edf2d1e4bfa7311a
Botsiture <bismillaha443@gmail.com>
2026-08-02 06:38:05 +0530

    Update check.py

--- shivu/modules/check.py
+++ shivu/modules/check.py
@@ -144,7 +144,6 @@ def owners_caption(char: Char, owners: List[Dict], page: int, gcount: int) -> st
     lines = [
         f"ㅤ🏆 {bold_sc('character owners')} 🏆","\n"
     ]
-    
     for i, o in enumerate(owners[start:end], start + 1):
         medal = {1: "🥇", 2: "🥈", 3: "🥉"}.get(i, f"<code>{i}.</code>")
         link = f"<b><a href='tg://user?id={o['id']}'>{escape(o['first_name'])}</a></b>"
