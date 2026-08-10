9e5d21d9bcff94e832de11a39aeb1b5493423d10
Botsiture <bismillaha443@gmail.com>
2026-08-10 23:53:43 +0530

    Update db.py

--- shivu/Database/db.py
+++ shivu/Database/db.py
@@ -1,18 +1,20 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-
+# Tera original MongoDB URL
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.getenv("DB_NAME", "GRABBING_YOUR_WAIFU")
 COLLECTION_NAME = "users"
 
-client = MongoClient(MONGO_URI)
+# Async client setup (Bot fast karne ke liye)
+client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 collection = db[COLLECTION_NAME]
 
-def get_user_data(user_id):
-    return collection.find_one({"user_id": user_id})
+# Async functions
+async def get_user_data(user_id):
+    return await collection.find_one({"user_id": user_id})
 
-def save_user_data(user_id, user_data):
-    collection.update_one({"user_id": user_id}, {"$set": user_data}, upsert=True)
+async def save_user_data(user_id, user_data):
+    await collection.update_one({"user_id": user_id}, {"$set": user_data}, upsert=True)
     print("User data saved successfully.")
