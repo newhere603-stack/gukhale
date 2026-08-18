c9b6e0b6b0ee419afd404eda32e489e2dba825b7
Botsiture <bismillaha443@gmail.com>
2026-08-18 06:02:25 +0530

    Change font size from 60 to 50 in wordgrid.py

--- shivu/modules/wordgrid.py
+++ shivu/modules/wordgrid.py
@@ -117,7 +117,7 @@ def create_grid_image(grid, placed_words, found_words, chat_id):
     img = Image.new('RGBA', (img_size, img_size), color=bg_color) 
     draw = ImageDraw.Draw(img)
 
-    font = get_bold_font(60)
+    font = get_bold_font(50)
 
     for r in range(size + 1):
         draw.line([(0, r*cell_size), (img_size, r*cell_size)], fill=line_color, width=3)
